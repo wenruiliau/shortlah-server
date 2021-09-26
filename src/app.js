d38182aeb2dcee29c6356babd25fb334fc5265a8
Wen Rui <wenrui@berkeley.edu>
2021-09-26 23:29:11 +0800

    enable cors

--- src/app.js
+++ src/app.js
@@ -27,11 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+app.use(cors());
 
 // connect DB
 const db = mongoose.connection;
